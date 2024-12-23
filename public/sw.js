0f1c93bcf3b7b4d56acb2184fb6e196e301dfd5c
negar <negar.karimnejad@gmail.com>
2024-12-23 16:07:47 +0330

    style app

---
+++ public/sw.js
@@ -0,0 +1,100 @@
+// const limitInCache = (key, size) => {
+//   caches.open(key).then((cache) => {
+//     cache.keys().then((keys) => {
+//       if (keys.length > size) {
+//         cache.delete(keys[0]).then(limitInCache(key, size));
+//       }
+//     });
+//   });
+// };
+
+// const cacheVersion = 3;
+
+// const activeCaches = {
+//   static: `static-v${cacheVersion}`,
+//   dynamic: `dynamic-v${cacheVersion}`,
+// };
+
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches.open(activeCaches["static"]).then((cache) => {
+//       cache.addAll([
+//         "/",
+//         "/fallback.html",
+//         "js/script.js",
+//         "js/app.js",
+//         "js/code.js",
+//         "css/style.css",
+//       ]);
+//     })
+//   );
+// });
+
+// self.addEventListener("activate", (event) => {
+//   const activeCacheNames = Object.values(activeCaches);
+//   event.waitUntil(
+//     caches.keys().then((cacheNames) => {
+//       return Promise.all(
+//         cacheNames.forEach((cacheName) => {
+//           if (!activeCacheNames.includes(cacheName)) {
+//             return caches.delete(cacheName); // :))
+//           }
+//         })
+//       );
+//     })
+//   );
+// });
+
+// self.addEventListener("fetch", (event) => {
+//   // 1. First Cache, second network
+
+//   event.respondWith(
+//     caches.match(event.request).then((response) => {
+//       if (response) {
+//         return response;
+//       } else {
+//         return fetch(event.request)
+//           .then((serverResponse) => {
+//             return caches.open([activeCaches["dynamic"]]).then((cache) => {
+//               cache.put(event.request, serverResponse.clone());
+//               // limitInCache(activeCaches["dynamic"], 18);
+//               return serverResponse;
+//             });
+//           })
+//           .catch((err) => {
+//             console.log(err);
+
+//             return caches.match("/fallback.html");
+//           });
+//       }
+//     })
+//   );
+
+//   // 2. Network Only
+//   // event.respondWith(fetch(event.request));
+
+//   // 3. Cache Only
+//   // event.respondWith(caches.match(event.request));
+
+//   // 4. First Network, second Cache
+//   // return event.respondWith(
+//   //   fetch(event.request)
+//   //     .then((response) => {
+//   //       return caches.open(activeCaches["dynamic"]).then((cache) => {
+//   //         cache.put(event.request, response.clone());
+//   //         return response;
+//   //       });
+//   //     })
+//   //     .catch((err) => {
+//   //       return caches.match(event.request);
+//   //     })
+//   // );
+// });
+
+// self.addEventListener("sync", (event) => {
+//   if (event.tag === "add-course") {
+//     addCourse();
+//   }
+// });
+
+// const addCourse = async () => {};
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import { useState } from "react";
+import { fetchWeather } from "./api/fetchWeather";
+import './App.css';
 
 export default function App() {
   const [query, setQuery] = useState("");
 
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+      console.log(data);
+    }
+  };
+
   return (
-    <div>
+    <div className="main-container">
       <input
         type="text"
         className="search"
         placeholder="Search ..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        onKeyDown={search}
       />
     </div>
   );
