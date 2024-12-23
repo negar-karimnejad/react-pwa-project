9b71fd7afb85674b3ca62f9e7e95f54cc412f2ab
negar <negar.karimnejad@gmail.com>
2024-12-23 20:46:42 +0330

    fixed error

--- vite.config.js
+++ vite.config.js
@@ -16,38 +16,8 @@ const pwaConfigs = {
     start_url: "/",
     icons: [
       {
-        src: "/icons/icon-48x48.png",
-        sizes: "48x48",
-        type: "/image/png",
-      },
-      {
-        src: "/icons/icon-72x72.png",
-        sizes: "72x72",
-        type: "/image/png",
-      },
-      {
-        src: "/icons/icon-128x128.png",
-        sizes: "128x128",
-        type: "/image/png",
-      },
-      {
-        src: "/icons/icon-152x152.png",
-        sizes: "152x152",
-        type: "/image/png",
-      },
-      {
-        src: "/icons/icon-192x192.png",
-        sizes: "192x192",
-        type: "/image/png",
-      },
-      {
-        src: "/icons/icon-256x256.png",
-        sizes: "256x256",
-        type: "/image/png",
-      },
-      {
-        src: "/icons/icon-512x512.png",
-        sizes: "512x512",
+        src: "/images/logo.png",
+        sizes: "1024x1024",
         type: "/image/png",
       },
     ],
