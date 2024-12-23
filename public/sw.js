62fdd50db3f1d563935a2e5bc93719d4b485b08c
negar <negar.karimnejad@gmail.com>
2024-12-23 18:20:00 +0330

    create sw file

--- public/sw.js
+++ public/sw.js
@@ -98,3 +98,43 @@
 // });
 
 // const addCourse = async () => {};
+
+const CACHE_NAME = "version-1";
+const urlsCache = ["/index.html", "offline.html"];
+
+// Insatll the SW
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsCache);
+    })
+  );
+});
+
+// Listen for requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
+    })
+  );
+});
+
+// Activate the SW
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAME);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
